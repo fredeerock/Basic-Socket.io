6bcd4cf1e1fa3fcbc59de0e9ee6dbc7bf5ae3b1b
Derick Ostrenko <dostrenko@gmail.com>
2014-06-21 00:40:56 -0500

    heartbeat

--- app.js
+++ app.js
@@ -57,11 +57,11 @@ console.log("Static file server running at => http://localhost:" + port);
 var io = require('socket.io')(app);
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  // socket.emit('news', { hello: 'world' });
 
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
 
   socket.on('the date', function (data) {
   	console.log(data);
